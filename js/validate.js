d7e26eb4a2d30aa61e10454d04e7885ba14627b5
twparamed <twparamed@gmail.com>
2025-04-21 13:38:45 +0800

    Add files via upload

---
+++ js/validate.js
@@ -0,0 +1,14 @@
+
+(() => {
+    'use strict';
+    const forms = document.querySelectorAll('.needs-validation');
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', e => {
+            if (!form.checkValidity()) {
+                e.preventDefault();
+                e.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+})();
